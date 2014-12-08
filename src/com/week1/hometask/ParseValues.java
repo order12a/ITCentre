d98ba2999b26bc5d26e8a4a6cc724a22c54d9717
Vladimir Uroshlev <vladimir.uroshlev@gmail.com>
2014-12-08 17:35:37 +0200

    Parsing almost done

--- src/com/week1/hometask/ParseValues.java
+++ src/com/week1/hometask/ParseValues.java
@@ -19,11 +19,18 @@ public class ParseValues {
 	public static void main(String[] args) throws FileNotFoundException {
 		String[][] pairs = ParseValues.parseData("data.txt");
 		for (int i = 0; i < pairs.length; i++) {
-			for (int j = 0; j < pairs[i].length; j++) {
-				System.out.println(pairs[i][j]);
-			}
+			
+				System.out.print(pairs[i][0] + "   ");
+				
+
 		}
+		
 		System.out.println();
+		
+		for (int i = 0; i < pairs.length; i++) {
+			System.out.print(pairs[i][1] + "   ");
+		}
+		
 	}
 
 	public static String[][] parseData(String fileName)
@@ -33,23 +40,55 @@ public class ParseValues {
 
 		FileReader fr = new FileReader(fileName);
 		Scanner scan = new Scanner(fr);
+		StringBuilder builder = new StringBuilder();
 
-		for (int i = 0; i < parsedData[i].length; i++) {
-			while (scan.hasNextLine()) {
-				scan.useDelimiter(";");
-				String line = scan.nextLine();
-				String[] dataPair = line.split("=");
-
-				for (int j = 0; j < parsedData[j].length; j++) {
-					System.out.println("Data = " + dataPair[j]);
-					parsedData[i][j] = dataPair[j];
-				}
-
-			}
+		while (scan.hasNextLine()) {
+			
+			builder = builder.append(scan.nextLine());
 		}
-
 		scan.close();
+//		String 
+		
+//		for (int i = 0; i < parsedData[i].length; i++) {
+//			while (scan.hasNextLine()) {
+//				scan.useDelimiter(";");
+//				String line = scan.nextLine();
+//				String[] dataPair = line.split("=");
+//
+//				for (int j = 0; j < parsedData[j].length; j++) {
+//					System.out.println("Data = " + dataPair[j]);
+//					parsedData[i][j] = dataPair[j];
+//				}
+//
+//			}
+//		}
 
-		return parsedData;
+//		scan.close();
+		String[][] targetArr = new String[10][2];
+		String result = builder.toString();
+		String[] arr = result.split(";");
+//		System.out.println(arr[0] + " " +  arr[1] +" " +  arr[2]);
+		
+		for (int i = 0; i < arr.length; i++) {
+			String[] newArr = arr[i].split("=");
+//			System.out.println(newArr[0] + " " + newArr[1]);
+			if (newArr[0] == null) {
+				break;
+			}else{
+				targetArr[i][0] = newArr[0];
+			}
+			
+			if (newArr[1].equals(null)) {
+				break;
+			} else {
+				targetArr[i][1] = newArr[1];
+			}
+			
+			
+			
+//			System.out.println(targetArr[i][0] + " " + targetArr[i][1]);
+		}
+		
+		return targetArr;
 	}
 }
